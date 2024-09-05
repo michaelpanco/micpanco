417d64c3981d618ce2c5a33088d98aaa08516772
Michael Panco <micpanco@gmail.com>
2024-09-05 17:22:11 +0800

    update resume

--- app/page.client.js
+++ app/page.client.js
@@ -39,7 +39,7 @@ export default function Home() {
           </div>
           <div className="pt-2">
             <Link
-              href="/assets/files/resume_v3.pdf"
+              href="/assets/files/resume_v3.1.1.pdf"
               className="hover:underline"
               target="_blank"
             >
